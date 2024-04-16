5a4d50920200a39eb658266a7aeec26833beb812
ExperienceV <132963842+ExperienceV@users.noreply.github.com>
2024-04-15 21:04:46 -0500

    API RestClient.
    ## 1.0.0
    
    - Initial version.
    -

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/login_form.tsx';
+import LoginPage from './expyFnt/login_form.tsx';
 import FourZeroFourPage from './pages/FZF.tsx';
-import RegisterPage from './pages/register_form.tsx';
+import RegisterPage from './expyFnt/register_form.tsx';
 import Home from './pages/home.tsx';
-import Test from './pages/test.tsx';
+import Test from './expyFnt/test.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
